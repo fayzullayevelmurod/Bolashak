b40313e306a6446eff2d65969f3f59c436a1ee12
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2023-11-12 18:22:58 +0500

    contact inprogress

---
+++ js/app.js
@@ -0,0 +1,24 @@
+var swiper = new Swiper(".licensesSlider", {
+    slidesPerView: 3,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        },
+        // when window width is >= 640px
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 10
+        },
+      },
+});
