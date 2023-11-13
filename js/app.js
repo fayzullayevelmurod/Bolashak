2d7b0fc5f345712d132da0ce20ece09b21a061bb
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2023-11-13 16:48:42 +0500

    index inprogress

--- js/app.js
+++ js/app.js
@@ -1,22 +1,69 @@
-let licenses = new Swiper(".licensesSlider", {
-    slidesPerView: 3,
+var swiper = new Swiper(".licensesSlider", {
+    slidesPerView: 1,
     spaceBetween: 30,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-    // breakpoints: {
-    //     320: {
-    //       slidesPerView: 2,
-    //       spaceBetween: 20
-    //     },
-    //     480: {
-    //       slidesPerView: 3,
-    //       spaceBetween: 30
-    //     },
-    //     768: {
-    //       slidesPerView: 3,
-    //       spaceBetween: 10
-    //     },
-    //   },
+    breakpoints: {
+        800: {
+          slidesPerView: 1,
+        },
+        1000: {
+          slidesPerView: 2,
+        },
+        1200: {
+          slidesPerView: 3,
+        },
+      },
+});
+
+var swiper = new Swiper(".newsSlider", {
+  slidesPerView: 4,
+  spaceBetween: 24,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1000: {
+      slidesPerView: 3,
+    },
+    1300: {
+      slidesPerView: 4,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+var swiper = new Swiper(".partenrsSlider", {
+  slidesPerView: 6,
+  spaceBetween: 24,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1000: {
+      slidesPerView: 4,
+    },
+    1400: {
+      slidesPerView: 6,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
