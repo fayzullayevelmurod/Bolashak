ada50c6cb10d59598ea0e77951621f76301161be
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2023-11-13 10:15:35 +0500

    pagination

--- js/app.js
+++ js/app.js
@@ -1,24 +1,22 @@
-var swiper = new Swiper(".licensesSlider", {
+let licenses = new Swiper(".licensesSlider", {
     slidesPerView: 3,
+    spaceBetween: 30,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-    breakpoints: {
-        // when window width is >= 320px
-        320: {
-          slidesPerView: 2,
-          spaceBetween: 20
-        },
-        // when window width is >= 480px
-        480: {
-          slidesPerView: 3,
-          spaceBetween: 30
-        },
-        // when window width is >= 640px
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 10
-        },
-      },
+    // breakpoints: {
+    //     320: {
+    //       slidesPerView: 2,
+    //       spaceBetween: 20
+    //     },
+    //     480: {
+    //       slidesPerView: 3,
+    //       spaceBetween: 30
+    //     },
+    //     768: {
+    //       slidesPerView: 3,
+    //       spaceBetween: 10
+    //     },
+    //   },
 });
